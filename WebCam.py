f83322b87ef4d003410f404c540d3e43c8518eb1
EsasAdam <48632551+esasadam06@users.noreply.github.com>
2020-04-06 21:31:57 +0300

    alpha

--- WebCam.py
+++ WebCam.py
@@ -1,23 +1,22 @@
+
+
 import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade-frontalface-default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade-eye.xml')
 people=0
+
+
 def detect(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     people = 0
-    x1=0
-    y1=0
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
         cv2.putText(frame, 'Detected', (x+w, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
         people = people + 1
     cv2.putText(frame, 'Detected People :' + str(people) + " ", (35, 35), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1,cv2.LINE_AA)
-
-    print(people)
     return frame
+
+
 video_capture = cv2.VideoCapture(0)
 while True:
     _, frame = video_capture.read()
